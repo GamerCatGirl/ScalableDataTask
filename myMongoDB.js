064451f52115941a1c9b10e2154a5aec9eebf344
Silken <silken_heynderickx@icloud.com>
2024-11-25 16:06:22 +0100

    sleeping

--- myMongoDB.js
+++ myMongoDB.js
@@ -37,24 +37,38 @@ function randomTimestamp() {
     return new Date(new Date() - Math.random() * (1e+12))
 }
 
-// TODO: add 1 million data to the database 
+function makeLightElm(timeStamp) {
+    const r = getRandomInt(255);
+    const g = getRandomInt(255);
+    const b = getRandomInt(255);
+    const lamp = getRandomInt(100);
+    const DeviceNameLamp = "Lamp " + lamp.toString();
+    const LightPercent = getRandomInt(100);
+    const color = "rgb( " + r + ", " + g + ", " + b + ")";
+    const DocLamp = makeDocLamp(DeviceNameLamp, timeStamp, LightPercent, color);
+    return DocLamp;
+}
+
+
 function populateDatabaseLights(amount, db) {
     const elements = new Array();
 
     for (let i = 0; i < amount; i++) {
         const timeStamp = randomTimestamp();
-        const r = getRandomInt(255);
-        const g = getRandomInt(255);
-        const b = getRandomInt(255);
-        const lamp = getRandomInt(100);
-        const DeviceNameLamp = "Lamp " + lamp.toString();
-        const LightPercent = getRandomInt(100);
-        const color = "rgb( " + r + ", " + g + ", " + b + ")";
-        const DocLamp = makeDocLamp(DeviceNameLamp, timeStamp, LightPercent, color);
+        const DocLamp = makeLightElm(timeStamp);
         elements.push(DocLamp);
     }
     db.insertMany(elements);
 
+}
+
+function makeSensorElm(timeStamp) {
+    const lamp = getRandomInt(100);
+    const DeviceNameSensor = "Sensor " + lamp.toString();
+    const Temperature = getRandomInt(150);
+    const Humidity = getRandomInt(100);
+    const DocSensor = makeDocSensor(DeviceNameSensor, timeStamp, Temperature, Humidity);
+    return DocSensor;
 }
 
 function populateDatabaseSensor(amount, db) {
@@ -62,11 +76,7 @@ function populateDatabaseSensor(amount, db) {
 
     for (let i = 0; i < amount; i++) {
         const timeStamp = randomTimestamp();
-        const lamp = getRandomInt(100);
-        const DeviceNameSensor = "Sensor " + lamp.toString();
-        const Temperature = getRandomInt(150);
-        const Humidity = getRandomInt(100);
-        const DocSensor = makeDocSensor(DeviceNameSensor, timeStamp, Temperature, Humidity);
+        const DocSensor = makeSensorElm(timeStamp);
         elements.push(DocSensor);
     }
     db.insertMany(elements);
@@ -82,40 +92,89 @@ function randomBoolean() {
     }
 }
 
+function makeElecElm(timeStamp) {
+    const plugg = getRandomInt(100);
+    const DeviceNameSensor = "Plugg " + plugg.toString();
+    const On = randomBoolean();
+    const Watt = getRandomInt(1000);
+    const DocPlugg = makeDocPlugg(DeviceNameSensor, timeStamp, On, Watt);
+    return DocPlugg;
+}
+
 function populateDatabaseElectricity(amount, db) {
     const elements = new Array();
 
     for (let i = 0; i < amount; i++) {
         const timeStamp = randomTimestamp();
-        const plugg = getRandomInt(100);
-        const DeviceNameSensor = "Plugg " + plugg.toString();
-        const On = randomBoolean();
-        const Watt = getRandomInt(1000);
-        const DocPlugg = makeDocPlugg(DeviceNameSensor, timeStamp, On, Watt);
+        const DocPlugg = makeElecElm(timeStamp);
         elements.push(DocPlugg);
     }
     db.insertMany(elements);
 
 }
 
+// taken from https://javascript.plainenglish.io/javascript-add-hours-to-date-6e3a39bb9345
+function addhours(hours) {
+    const date = new Date();
+    const hoursToAdd = hours * 60 * 60 * 1000;
+    date.setTime(date.getTime() + hoursToAdd);
+    return date;
+}
+
+async function insertInto(elm, db) {
+    await db.insertOne(elm)
+}
+
+async function populateDatabases(dbLamp, dbSensor, dbElec) {
+    const startTime = new Date();
+    const stopTime = addhours(24);
+
+    let added = false;
+    let currentTime = startTime;
+
+    while (currentTime < stopTime) {
+        if (!added) {
+            //1. add data 
+            const docLamp = makeLightElm(currentTime);
+            insertInto(docLamp, dbLamp);
+            const docSensor = makeSensorElm(currentTime);
+            insertInto(docSensor, dbSensor);
+            const docElec = makeElecElm(currentTime);
+            insertInto(docElec, dbElec);
+            added = true;
+            //2. sleep 5seconds 
+            console.log("start sleeping...")
+            setTimeout(function () {
+                //3set currentTime
+                console.log("sleep done");
+                currentTime = new Date();
+                added = false;
+            }, 5);
+        }
+
+    }
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
         // Send a ping to confirm a successful connection
         const DB = client.db("TimeSeries");
 
-        const lampen = DB.collection("lamps");
-        const sensors = DB.collection("sensors");
-        const elictricity = DB.collection("Wall Pluggs");
+        const lampen = await DB.collection("lamps");
+        const sensors = await DB.collection("sensors");
+        const elictricity = await DB.collection("Wall Pluggs");
 
         //populateDatabaseLights(1000, lampen);
         //populateDatabaseElectricity(1000, elictricity);
         //populateDatabaseSensor(1000, sensors);
 
+        await populateDatabases(lampen, sensors, elictricity);
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
@@ -123,13 +182,24 @@ async function run() {
 }
 run().catch(console.dir);
 
+// TODO: let code run 24 hours to populate database 
+// TODO: populate database to fit x amount data 
+// use that data for the benchmarks 
+
 //setup benchmarks 
+suite
+    .add("write data to database", function () {
+        //TODO: write data to database 
+    })
+    .add("fetch all data of same id", function () {
+        //TODO: fetch data 
+    })
+    .add("get latest data (last 1h)", function () {
+        //TODO: fetch latest data 
+    })
+    .on('complete', function () {
+        //TODO: print the results 
+    })
 
-// TODO: vary the intervals where new data will be written
-
-
-
-// TODO: refresh the view by reading the latest value (morst recent timestamp) every 
-// [1s, 5s, 10s, 30s, 60s, 90s]
 
 // TODO: find some database queries that are more difficult to execute 
